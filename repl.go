7dbb8b9d2fcbed43653f48f8646fcf8f3082eb52
Kurlgargyey <nahsirkivar@gmail.com>
2025-02-04 15:12:56 +0100

    getting pokemon areas using the pokeapi go library

--- repl.go
+++ repl.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/mtslzr/pokeapi-go"
 )
 
 type cliCommand struct {
@@ -12,7 +14,12 @@ type cliCommand struct {
 	callback    func() error
 }
 
+type config struct {
+	locationPage int
+}
+
 var commands map[string]cliCommand
+var cfg config
 
 func init() {
 	commands = make(map[string]cliCommand)
@@ -27,7 +34,17 @@ func init() {
 		description: "Displays a help message",
 		callback:    commandHelp,
 	}
-
+	commands["map"] = cliCommand{
+		name:        "map",
+		description: "Displays the next 20 locations in the world of pokemon",
+		callback:    commandMap,
+	}
+	commands["mapb"] = cliCommand{
+		name:        "mapb",
+		description: "Displays the previous 20 locations in the world of pokemon",
+		callback:    commandMapB,
+	}
+	cfg.locationPage = 0
 }
 
 func cleanInput(text string) []string {
@@ -43,12 +60,39 @@ func commandExit() error {
 }
 
 func commandHelp() error {
-	commandNames := []string{"help", "exit"}
+	commandNames := []string{"map", "mapb", "help", "exit"}
 
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Print("Usage:\n\n")
 	for _, cmd := range commandNames {
 		fmt.Printf("%s - %s\n", commands[cmd].name, commands[cmd].description)
+	}
+	return nil
+}
+
+func commandMap() error {
+	locs, err := pokeapi.Resource("location-area", cfg.locationPage*20)
+	if err != nil {
+		return err
+	}
+	for _, loc := range locs.Results {
+		fmt.Println(loc.Name)
+	}
+	cfg.locationPage++
+	return nil
+}
+func commandMapB() error {
+	if cfg.locationPage <= 1 {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+	cfg.locationPage-=2
+	locs, err := pokeapi.Resource("location-area", cfg.locationPage*20)
+	if err != nil {
+		return err
+	}
+	for _, loc := range locs.Results {
+		fmt.Println(loc.Name)
 	}
 	return nil
 }
